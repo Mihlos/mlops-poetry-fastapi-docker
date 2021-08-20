1b0423c3fd9b04186f1c45200134dcebef8a757f
Miguel López <miguel.mish@gmail.com>
2021-08-20 18:29:55 +0200

    docker working with example

---
+++ mlops/app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from mlops.routers import routers
+
+app = FastAPI()
+app.include_router(routers.router, prefix='/iris')
+
+
+@app.get('/healthcheck', status_code=200)
+async def healthcheck():
+    return 'Iris classifier is all ready to go!'
