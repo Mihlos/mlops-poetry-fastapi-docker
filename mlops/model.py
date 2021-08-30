1cc801d5ac325d32f419f44aa6ec0838a7887fd9
Mihlos <miguel.mish@gmail.com>
2021-08-30 20:25:07 +0200

    load model.pkl

--- mlops/model.py
+++ mlops/model.py
@@ -1,19 +1,17 @@
 import numpy as np
 import pickle
-from sklearn.datasets import load_iris
-from sklearn.neighbors import KNeighborsClassifier
 
 from mlops.schemas.iris import IrisInput
 
 
 class IrisClassifier:
     def __init__(self):
-        self.X, self.y = load_iris(return_X_y=True)
-        self.clf = self.train_model()
+        self.clf = self.load_model()
         self.iris_type = {0: "setosa", 1: "versicolor", 2: "virginica"}
 
-    def train_model(self) -> KNeighborsClassifier:
-        return KNeighborsClassifier(n_neighbors=3).fit(self.X, self.y)
+    def load_model(self):
+        with open("model.pkl") as model:
+            pickle.load(model)
 
     def classify_iris(self, features: dict):
         X = IrisInput(**features)
@@ -26,11 +24,7 @@ class IrisClassifier:
             "probability": round(max(prediction[0]), 2),
         }
 
-    def save_model(self):
-        with open("model.pkl", "wb") as model:
-            pickle.dump(self.clf, model)
-
 
 if __name__ == "__main__":
     model = IrisClassifier()
-    model.save_model()
+    print(model.clf)
